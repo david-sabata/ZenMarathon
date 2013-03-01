ff701515469915192dd0cd82b126dc24143fe0b7
TomasKimer <tomas.kimer@gmail.com>
2013-03-01 19:03:12 +0100

    animace

--- zen-desktop/src/cz/emo4d/zen/Main.java
+++ zen-desktop/src/cz/emo4d/zen/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "zen";
-		cfg.useGL20 = false;
+		cfg.useGL20 = true;
 		cfg.width = 480;
 		cfg.height = 320;
 		
