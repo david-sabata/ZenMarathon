f48912dabd40ef8716fd97e2c0745de13d8869c9
TomasKimer <tomas.kimer@gmail.com>
2013-03-02 18:55:08 +0100

    cerveny efekt zabiti

--- zen/src/cz/emo4d/zen/gameplay/Enemy.java
+++ zen/src/cz/emo4d/zen/gameplay/Enemy.java
@@ -14,7 +14,7 @@ public class Enemy extends Mob {
 		super();
 		this.position = position;
 
-		this.effect = new Effect(new Texture(Gdx.files.internal("data/effects/player_sheet.png")), 6, 8, 0.1f, 1, 0, 0);
+		this.effect = new Effect(new Texture(Gdx.files.internal("data/effects/enemy_sheet.png")), 6, 8, 0.1f, 1, 0, 0);
 		WIDTH = 1 / 32f * (effect.width - 3);
 		HEIGHT = 1 / 32f * (effect.height - 15);
 
