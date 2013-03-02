62acf0b2c2616ceef478013c166e8359aeb91388
Tomsa <xholom01@stud.fit.vutbr.cz>
2013-03-02 01:48:55 +0100

    several fixes

--- mcontrol/src/cz/emo4d/mcontrol/ControlActivity.java
+++ mcontrol/src/cz/emo4d/mcontrol/ControlActivity.java
@@ -11,23 +11,23 @@ import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ControlActivity extends Activity {
 
 	private final String LOG_TAG = "ZenMobileControl";
 	private int arrowImgXY[];
-	private int btnAXY[];
-	private int btnBXY[];
-	
+
+
 	private boolean mServiceBound = false;
 	private NetService mService = null;
-	
+
 	int lastX;
 	int lastY;
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +39,32 @@ public class ControlActivity extends Activity {
 
 		setContentView(R.layout.activity_control);
 	}
-	
+
 	@Override
 	public void onStart() {
 		super.onStart();
 
-		getApplicationContext().bindService(new Intent(this, NetService.class), netServiceConnection, Context.BIND_AUTO_CREATE);
+		getApplicationContext().bindService(new Intent(this, NetService.class),
+				netServiceConnection, Context.BIND_AUTO_CREATE);
+
+		final Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if (mServiceBound) {
+					mService.sendControlEvent(EventTypes.PRESS_A, 0, 0);
+				}
+			}
+		});
+		
+		final Button button2 = (Button) findViewById(R.id.button2);
+		button2.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if (mServiceBound) {
+					mService.sendControlEvent(EventTypes.PRESS_B, 0, 0);
+				}
+			}
+		});
+
 	}
 
 	@Override
@@ -52,7 +72,6 @@ public class ControlActivity extends Activity {
 		super.onStop();
 
 	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,76 +87,46 @@ public class ControlActivity extends Activity {
 		ImageView arrowController = (ImageView) findViewById(R.id.imageView1);
 		arrowImgXY = new int[2];
 		arrowController.getLocationOnScreen(arrowImgXY);
-		
-		ImageView BtnAController = (ImageView) findViewById(R.id.imageViewBtnA);
-		btnAXY = new int[2];
-		BtnAController.getLocationOnScreen(btnAXY);
-		
-		ImageView BtnBController = (ImageView) findViewById(R.id.imageViewBtnB);
-		btnBXY = new int[2];
-		BtnBController.getLocationOnScreen(btnBXY);
-		
-		
+
 	}
 
 	public boolean onTouchEvent(MotionEvent event) {
 		int eventX = (int) event.getX();
 		int eventY = (int) event.getY();
-		
+
 		if ((event.getAction() == MotionEvent.ACTION_DOWN)
 				|| (event.getAction() == MotionEvent.ACTION_MOVE)) {
-			
-			int xOnField = eventX - arrowImgXY[0] - 130; // 130 - picture center
-			int yOnField = eventY - arrowImgXY[1] - 130;
-			
-			if ((xOnField < 100) && (yOnField < 100) && (xOnField > -100) && (yOnField > -100)) {
+
+			int xOnField = eventX - arrowImgXY[0] - 250; // 130 - picture
+													// center
+			int yOnField = eventY - arrowImgXY[1] - 250;
+
+			if ((xOnField < 250) && (yOnField < 250) && (xOnField > -250)
+					&& (yOnField > -250)) {
 				// arrows
-				//Log.i(LOG_TAG, "on arrows (x, y) = " + Integer.toString(xOnField)
-				//		+ "   " + Integer.toString(yOnField));
+				//Log.i(LOG_TAG,
+				//		"on arrows (x, y) = " + Integer.toString(xOnField)
+				//				+ "   " + Integer.toString(yOnField));
 				if (mServiceBound) {
-					mService.sendControlEvent(EventTypes.MOVE, xOnField, yOnField);
+					mService.sendControlEvent(EventTypes.MOVE, xOnField,
+							yOnField);
 				}
-				
+
 				return super.onTouchEvent(event);
 			}
-			
-			xOnField = eventX - btnAXY[0];
-			yOnField = eventY - btnAXY[1];
-			
-			if (((xOnField < 150) && (yOnField < 150) && (xOnField > 0) && (yOnField > 0))) {
-				Log.i(LOG_TAG, "Button A");
-				if (mServiceBound) {
-					mService.sendControlEvent(EventTypes.PRESS_A, 0, 0);
-				}
-				
-				
-				return super.onTouchEvent(event);
-			}
-			
-			xOnField = eventX - btnBXY[0];
-			yOnField = eventY - btnBXY[1];
-			
-			if (((xOnField < 150) && (yOnField < 150) && (xOnField > 0) && (yOnField > 0))) {
-				Log.i(LOG_TAG, "Button B");
-				if (mServiceBound) {
-					mService.sendControlEvent(EventTypes.PRESS_B, 0, 0);
-				}
-				return super.onTouchEvent(event);
-			}
-			
+
 		}
-		
+
 		if (event.getAction() == MotionEvent.ACTION_UP) {
 			if (mServiceBound) {
 				mService.sendControlEvent(EventTypes.MOVE, 0, 0);
 			}
 			Log.i(LOG_TAG, "End of gesture");
 		}
-		
+
 		return super.onTouchEvent(event);
 	}
-	
-	
+
 	private ServiceConnection netServiceConnection = new ServiceConnection() {
 
 		@Override
@@ -147,13 +136,13 @@ public class ControlActivity extends Activity {
 			mService = binder.getService();
 			mServiceBound = true;
 
-			//ExecTask exe = new ExecTask();
-			//exe.execute();
-			Log.i(LOG_TAG,"Service bound");
-			
+			// ExecTask exe = new ExecTask();
+			// exe.execute();
+			Log.i(LOG_TAG, "Service bound");
+
 			mService.runAutoDiscovery();
 			mService.openConnection();
-			
+
 		}
 
 		@Override
